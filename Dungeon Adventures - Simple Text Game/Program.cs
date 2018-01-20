3aa436ee50e60b4965252677ac0845cf5ea14ccc
MaxWarman <33585291+MaxWarman@users.noreply.github.com>
2018-01-20 21:55:19 +0100

    Program cs - basics

--- Dungeon Adventures - Simple Text Game/Program.cs
+++ Dungeon Adventures - Simple Text Game/Program.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Dungeon_Adventures___Simple_Text_Game.Classes;
+using System.IO;
 
 namespace Dungeon_Adventures___Simple_Text_Game
 {
@@ -10,6 +12,30 @@ namespace Dungeon_Adventures___Simple_Text_Game
     {
         static void Main(string[] args)
         {
+            // Creating rooms
+            Random rand = new Random();
+            List<Dungeon> rooms = new List<Dungeon>();
+            rooms = createRooms(ref rooms);
+
+            // Creating players equipment
+            List<Item> eq = new List<Item>();
+
+            Console.ReadKey();
+        }
+
+        static List<Dungeon> createRooms(ref List<Dungeon> rooms)
+        {
+            string roomDescription;
+
+            // Room 0,0
+            roomDescription = "Test description of first room";
+            rooms.Add(new Dungeon(0, 0, roomDescription, false));
+
+            // Room 0,1
+            roomDescription = "Test description of second room";
+            rooms.Add(new Dungeon(0, 1, roomDescription, false));
+
+            return rooms;
         }
     }
 }
