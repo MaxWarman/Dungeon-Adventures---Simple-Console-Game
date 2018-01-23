51e0fe97f5e21deee874ca9bcda977793de8a91d
MaxWarman <33585291+MaxWarman@users.noreply.github.com>
2018-01-23 19:39:03 +0100

    Program.cs - tried to add moving menu, gave up

--- Dungeon Adventures - Simple Text Game/Program.cs
+++ Dungeon Adventures - Simple Text Game/Program.cs
@@ -20,6 +20,15 @@ namespace Dungeon_Adventures___Simple_Text_Game
             // Creating players equipment
             List<Item> eq = new List<Item>();
 
+            // Game Menu
+
+            //showGameMenu();
+            Console.WriteLine("Dungeon Adventures - The Text Game");
+            //int znak = Console.ReadKey().KeyChar;
+            //if (znak == Convert.ToInt32(ConsoleKey.UpArrow))
+            
+
+
             Console.ReadKey();
         }
 
@@ -36,6 +45,38 @@ namespace Dungeon_Adventures___Simple_Text_Game
             rooms.Add(new Dungeon(0, 1, roomDescription, false));
 
             return rooms;
+        }
+        static void showGameMenu()
+        {
+            do
+            {
+                int actIndex = 0;
+                string[] menuOptions = new string[4];
+                menuOptions[0] = "---    1. New Game     ---";
+                menuOptions[1] = "---    2. Load Game    ---";
+                menuOptions[2] = "---    3. Credits      ---";
+                menuOptions[3] = "---    4. Exit         ---";
+
+                for (int i = 0; i < menuOptions.Length; i++)
+                {
+                    if (i != actIndex)
+                    {
+                        Console.BackgroundColor = ConsoleColor.White;
+                        Console.ForegroundColor = ConsoleColor.Black;
+                        Console.WriteLine(menuOptions[i]);
+                        Console.BackgroundColor = ConsoleColor.Black;
+                        Console.ForegroundColor = ConsoleColor.White;
+                    }
+                    else
+                    {
+                        Console.WriteLine(menuOptions[i]);
+                    }
+                }
+
+                int character = int.Parse(Console.ReadKey().ToString());
+
+
+            } while (true);
         }
     }
 }
